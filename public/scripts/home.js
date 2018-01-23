ac1e559f393633ed413a4beacb7c5ec3b0c6bd59
brycedai <brycedai@utexas.edu>
2018-01-23 22:42:31 +0000

    increase Android functionality

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -4,11 +4,23 @@ yourLocation.addEventListener("click", getLocation);
 
 
 function getLocation() {
+    var ua = navigator.userAgent.toLowerCase(),
+    isAndroid = ua.indexOf("android") > -1,
+    geoTimeout = isAndroid ? '15000' : '1000';
+    
+    function success(location){
+        document.getElementById("location").value = location.coords.latitude + ", " + location.coords.longitude;
+    }
+    
+    function error (err){
+        console.log(err);
+    }
+        
     if ("geolocation" in navigator) {
         /* geolocation is available */
-        navigator.geolocation.getCurrentPosition(function(location) {
-            document.getElementById("location").value = location.coords.latitude + ", " + location.coords.longitude;
-        });
-    } 
+        navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true, maximumAge: 3000, timeout:geoTimeout});
+    } else {
+        error("Location services must be enabled to use this");
+    }
     
 }
